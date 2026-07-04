34cd4ae8eaad0b29380ac1adebabbca7fdfb6131
Bobby <joz548@usask.ca>
2026-07-04 15:03:07 -0600

    Set BMP280 Barometer I2C address to 0x77

--- src/Global.cpp
+++ src/Global.cpp
@@ -7,7 +7,7 @@ WDT_T4<WDT2> WatchDog;
 
 // ==== Sensors ====
 LIS3MDL Magnetometer(GetSysTick, 0x1E, HAL::I2C_WIRE);
-BMP280 Barometer {GetSysTick, 0, HAL::I2C_WIRE};
+BMP280 Barometer {GetSysTick, 0x77, HAL::I2C_WIRE};
 LSM6 AccelGyro(GetSysTick, 0x6B, HAL::I2C_WIRE);
 
 ra::Logger& Logger = ra::Logger::Instance();
